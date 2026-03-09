989684345203e80e28e11df9e95306cd4ce9ae6a
farmerng46-spec <farmerng46@gmail.com>
2026-03-09 10:20:03 +0100

    Update ClearLagTask.java

--- src/main/java/me/emmy/tulip/task/ClearLagTask.java
+++ src/main/java/me/emmy/tulip/task/ClearLagTask.java
@@ -3,27 +3,26 @@ package me.emmy.tulip.task;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
 import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  * @author Emmy
  * @project FFA
  * @date 07/09/2024 - 15:10
+ * Optimized for high-performance FFA servers.
  */
 public class ClearLagTask extends BukkitRunnable {
 
     @Override
     public void run() {
         for (World world : Bukkit.getWorlds()) {
-            for (Entity entity : world.getEntities()) {
-                if (entity instanceof Arrow) {
-                    Arrow arrow = (Arrow) entity;
-                    if (arrow.isOnGround()) {
-                        arrow.remove();
-                    }
+            // Ottenere direttamente la collezione di frecce è molto più veloce 
+            // che ciclare tutte le entità del mondo.
+            world.getEntitiesByClass(Arrow.class).forEach(arrow -> {
+                if (arrow.isOnGround() || arrow.isDead()) {
+                    arrow.remove();
                 }
-            }
+            });
         }
     }
 }
