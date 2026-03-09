6cf4357807d17ec66d399529b8c206135e8803fc
farmerng46-spec <farmerng46@gmail.com>
2026-03-09 16:33:23 +0100

    Update ReloadCommand.java

--- src/main/java/me/emmy/tulip/command/ReloadCommand.java
+++ src/main/java/me/emmy/tulip/command/ReloadCommand.java
@@ -13,13 +13,31 @@ import org.bukkit.command.CommandSender;
  * @date 28/07/2024 - 23:57
  */
 public class ReloadCommand extends BaseCommand {
+
     @Override
-    @Command(name = "tulip.reload", aliases = {"trl"}, inGameOnly = false)
+    @Command(name = "tulip.reload", permission = "tulip.admin", aliases = {"trl"}, inGameOnly = false)
     public void onCommand(CommandArgs command) {
         CommandSender sender = command.getSender();
+        Tulip plugin = Tulip.getInstance();
 
-        sender.sendMessage(CC.translate("&cReloading Tulip..."));
-        Tulip.getInstance().getConfigService().reloadConfigs();
-        sender.sendMessage(CC.translate("&aTulip has been reloaded."));
+        sender.sendMessage(CC.translate("&c&l[Tulip] &7Inizio ricarica dei moduli..."));
+
+        try {
+            // 1. Ricarica i file YAML nella RAM
+            plugin.getConfigService().reloadConfigs();
+
+            // 2. Ricarica i Repository (Fondamentale per aggiornare kit/arene senza riavviare)
+            // Assicurati che questi metodi chiamino internamente i nuovi file ricaricati
+            plugin.getArenaRepository().loadArenas();
+            plugin.getKitRepository().loadKits();
+            plugin.getFfaSpawnHandler().loadFFASpawn();
+            
+            // 3. Notifica il successo
+            sender.sendMessage(CC.translate("&a&l[Tulip] &7Tutti i moduli e le configurazioni sono stati ricaricati con successo!"));
+            
+        } catch (Exception e) {
+            sender.sendMessage(CC.translate("&4&l[!] &cErrore critico durante il reload. Controlla la console."));
+            e.printStackTrace();
+        }
     }
 }
