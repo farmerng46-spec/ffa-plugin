a9c1396e2b20ea8bc33787d5a434acc8d38fc678
farmerng46-spec <farmerng46@gmail.com>
2026-03-09 16:33:53 +0100

    Update TulipCommand.java

--- src/main/java/me/emmy/tulip/command/TulipCommand.java
+++ src/main/java/me/emmy/tulip/command/TulipCommand.java
@@ -13,19 +13,29 @@ import org.bukkit.command.CommandSender;
  * @date 27/07/2024 - 17:55
  */
 public class TulipCommand extends BaseCommand {
+
     @Command(name = "tulip", inGameOnly = false)
     @Override
     public void onCommand(CommandArgs command) {
         CommandSender sender = command.getSender();
+        Tulip plugin = Tulip.getInstance();
+        
+        // Definiamo un colore principale (Azzurro Moderno/Viola) per tutto il core
+        String primary = "&#d48cf0"; // Un viola pastello che sta bene con il nome "Tulip"
+        String secondary = "&7";
+        String dot = "&f▢";
 
         sender.sendMessage("");
-        sender.sendMessage(CC.translate("&d======================================"));
-        sender.sendMessage(CC.translate(" &d&lTULIP FFA CORE"));
-        sender.sendMessage(CC.translate("  &e▢ Author: &d" + Tulip.getInstance().getDescription().getAuthors().get(0)));
-        sender.sendMessage(CC.translate("  &e▢ Version: &d" + Tulip.getInstance().getDescription().getVersion()));
-        sender.sendMessage(CC.translate("  &e▢ Github: &dhttps://github.com/hmEmmy/Tulip"));
-        sender.sendMessage(CC.translate("  &e▢ Discord: &dhttps://discord.gg/eT4B65k5E4"));
-        sender.sendMessage(CC.translate("&d======================================"));
+        sender.sendMessage(CC.translate(primary + "&l" + CC.SB_BAR));
+        sender.sendMessage(CC.translate(" " + primary + "&lTULIP FFA CORE &8- " + secondary + "Advanced FFA Solution"));
+        sender.sendMessage("");
+        sender.sendMessage(CC.translate("  " + primary + dot + " &eAuthor: " + secondary + plugin.getDescription().getAuthors().get(0)));
+        sender.sendMessage(CC.translate("  " + primary + dot + " &eVersion: " + secondary + plugin.getDescription().getVersion()));
+        sender.sendMessage("");
+        sender.sendMessage(CC.translate("  " + primary + dot + " &eGithub: " + secondary + "github.com/hmEmmy/Tulip"));
+        sender.sendMessage(CC.translate("  " + primary + dot + " &eDiscord: " + secondary + "discord.gg/eT4B65k5E4"));
+        sender.sendMessage("");
+        sender.sendMessage(CC.translate(primary + "&l" + CC.SB_BAR));
         sender.sendMessage("");
     }
 }
